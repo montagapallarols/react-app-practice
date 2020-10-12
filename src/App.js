43a92e1966406fa432df55f30c25aa6de1da22b9
montipallarols <montaga.pallarols@gmail.com>
2020-10-12 13:37:45 +0200

    Changed the title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello from Codaisseur</h1>
       <p>Today I'm learning about React!</p>
     </div>
   );
